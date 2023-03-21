4f90d7417783ba85a38b519c82c0900b75d7b1d0
Akshay Boraste <borasteak877@gmail.com>
2023-03-21 13:02:12 +0530

    Update Main.java

--- Intersection_of_two_arrays/src/Main.java
+++ Intersection_of_two_arrays/src/Main.java
@@ -7,34 +7,35 @@ public class Main {
 	public static int[] intrscnOfTwoArrays(int[] arr1, int[] arr2) {
 		
 		Set<Integer> set1 = new HashSet<>();
-        for (int i : arr1) {
-            set1.add(i);
-        }
-        
-        Set<Integer> set2 = new HashSet<>();
-        for (int i : arr2) {
-            set2.add(i);
-        }
-        
-        //Removed Duplicates using Hashset..
-        Set<Integer> set3 = new HashSet<>();
-        
-        /*checking whether elements of 1st Hashset is 
-         present in the 2nd Hashset or not*/
-        for (Integer var : set1) {  
-            if (set2.contains(var)) {
-                set3.add(var);
-            }
-        }
-        int[] arr = new int[set3.size()];
-        int j = 0;
-        for (Integer val : set3) {
-            arr[j] = val.intValue();
-            j++;
-        }
-        return arr;
+		for (int i : arr1) {
+		    set1.add(i);
+		}
+
+		Set<Integer> set2 = new HashSet<>();
+		for (int i : arr2) {
+		    set2.add(i);
+		}
+
+		//Removed Duplicates using Hashset..
+		Set<Integer> set3 = new HashSet<>();
+
+		/*checking whether elements of 1st Hashset is 
+		 present in the 2nd Hashset or not*/
+		for (Integer var : set1) {  
+		    if (set2.contains(var)) {
+			set3.add(var);
+		    }
+		}
+		
+		int[] arr = new int[set3.size()];
+		int j = 0;
+		for (Integer val : set3) {
+		    arr[j] = val.intValue();
+		    j++;
+		}
+		return arr;
        
-    }
+      }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

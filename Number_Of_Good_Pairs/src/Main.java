925ac356f37cadc7b4c13d9a794cba17d6cc7e68
Akshay Boraste <borasteak877@gmail.com>
2023-03-31 18:09:52 +0530

    number of good pairs solved

--- Number_Of_Good_Pairs/src/Main.java
+++ Number_Of_Good_Pairs/src/Main.java
@@ -2,45 +2,52 @@
 
 public class Main {
 	
-//	public static int goodPairs(int[] nums) {
-//        int count =0 ;
-//        int n = nums.length;
-//        for(int i=0 ; i<n ; i++){
-//            for(int j = i+1;j<n;j++){
-//                if(nums[i]==nums[j]){
-//                    count++;
-//                }
-//            }
-//        }
-//        return count;
-//    }
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		int[] arr = {1,2,3,1,1,3};
-//		System.out.println(goodPairs(arr));
-//	}
-
-	// TC: O(n^2); SC: O(1)
-	
-	public static int goodPairs(int[] nums) {
+ 	//APPROACH-1 ==> BRUTE FORCE
+	============================
 		
-        int count = 0;
-		int[] arr = new int[101];
-		  
-		for(int x : nums) {
-			count+=arr[x]++; 
-		}
-		
-		return count;
-    }
+    public static int goodPairs(int[] nums) {
+       int count =0 ;
+       int n = nums.length;
+       for(int i=0 ; i<n ; i++){
+           for(int j = i+1;j<n;j++){
+               if(nums[i]==nums[j]){
+                   count++;
+               }
+           }
+       }
+       return count;
+   }
 
    public static void main(String[] args) {
 	// TODO Auto-generated method stub
        int[] arr = {1,2,3,1,1,3};
-	   System.out.println(goodPairs(arr));
+	System.out.println(goodPairs(arr));
+   }
+
+   // TC: O(n^2); SC: O(1)
+	
+------------------------------------------------------------------------------------------------------------	
+	//APPROACH-2
+	============
+		
+    public static int goodPairs(int[] nums) {
+		
+        int count = 0;
+	int[] arr = new int[101];
+		  
+	for(int x : nums) {
+	    count+=arr[x]++; 
+	}
+		
+	return count;
+     }
+
+   public static void main(String[] args) {
+	// TODO Auto-generated method stub
+       int[] arr = {1,2,3,1,1,3};
+       System.out.println(goodPairs(arr));
    }
    
-	// TC: O(n); SC: O(n)
+   // TC: O(n); SC: O(n)
 	
 }
